9a28cb888e846e00c7fab24f167aaf75f264f031
kang162002 <6334540@naver.com>
2025-07-09 14:14:05 +0900

    Requset 요청 URL (컨트롤러)/BoardController 수정

--- src/main/java/com/app/controller/study_requst/BoardController.java
+++ src/main/java/com/app/controller/study_requst/BoardController.java
@@ -4,9 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.controller.HomeController;
+
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	
+	
+	 private final HomeController homeController;
+
+	    BoardController(HomeController homeController) {
+	        this.homeController = homeController;
+	    }
 
 	@RequestMapping("/faq")	//	/board/faq
 	public String faq() {
