093e03fa33d7b5133cdaf3eb93f9344e8951de5a
kang162002 <6334540@naver.com>
2025-07-09 15:07:26 +0900

    연습1 URL 맵핑하기

---
+++ src/main/java/com/app/controller/study/practice/practice01/Practice01Controller.java
@@ -0,0 +1,31 @@
+package com.app.controller.study.practice.practice01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Practice01Controller {
+	
+	
+//	@RequestMapping("/practice01/main")
+	@RequestMapping("/main")
+	public String main() {
+		return "practice/practice01/main";
+		
+	}
+	
+	@RequestMapping(value= "/product/mouse", method =RequestMethod.GET)
+	public String mouse() {
+		return "practice/practice01/product/muse";
+	}
+	
+	@RequestMapping("/practice01/product/keyboard")
+	public String keyboard() {
+		return "practice/practice01/product/keyboard";
+	}
+	
+	
+	
+
+}
